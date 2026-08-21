a210568344089910d7404ba92cceaeb5562a8503
aheddareamine <amine.aheddare-etu@etu.univh2c.ma>
2026-08-21 19:28:04 +0100

    minor fixes for overall code

--- src/header/core.h
+++ src/header/core.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   core.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaheddar <aaheddar@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/21 01:22:27 by aaheddar          #+#    #+#             */
+/*   Updated: 2026/08/21 10:05:11 by aaheddar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CORE_H
 # define CORE_H
 
@@ -5,52 +17,60 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <sys/time.h>
 # include <unistd.h>
-#include <sys/time.h>
 
-typedef struct timeval t_time;
+typedef struct timeval	t_time;
 
 typedef enum e_scheduler
 {
 	FIFO,
 	EDF
-}					t_scheduler;
+}						t_scheduler;
 
 typedef enum e_coder_state
 {
 	COMPILING,
 	DEBUGGING,
 	REFACTORING
-}					t_coder_state;
+}						t_coder_state;
 
 typedef struct parameters
 {
-	int				number_of_coders;
-	int				time_to_burnout;
-	int				time_to_compile;
-	int				time_to_debug;
-	int				time_to_refactor;
-	int				number_of_compiles_required;
-	int				dongle_cooldown;
-	t_scheduler		scheduler;
-}					t_parameters;
+	int					number_of_coders;
+	int					time_to_burnout;
+	int					time_to_compile;
+	int					time_to_debug;
+	int					time_to_refactor;
+	int					number_of_compiles_required;
+	int					dongle_cooldown;
+	t_scheduler			scheduler;
+}						t_parameters;
 
 typedef struct coder
 {
-	int				index;
-	int				compilation_number;
-	t_coder_state	state;
-	pthread_t		*thread;
-}					t_coder;
+	int					index;
+	int					compilation_number;
+	t_coder_state		state;
+	pthread_t			*thread;
+}						t_coder;
 
 typedef struct dongle
 {
-	int				taken;
-	int				index;
-}					t_dongle;
+	int					index;
+	int					cooldow;
+	int					taken;
+}						t_dongle;
 
-t_parameters		*parse(int argc, char **args);
-long				gettimestamp(void);
-void				set_start_time(void);
+typedef struct simulation
+{
+	int					nb_coders;
+	t_coder				*coders;
+	t_dongle			*dongles;
+}						t_simulation;
+
+t_parameters			*parse(int argc, char **args);
+long					gettimestamp(void);
+void					set_start_time(void);
 
 #endif
--- src/utils/time_stamp.c
+++ src/utils/time_stamp.c
@@ -1,25 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time_stamp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaheddar <aaheddar@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/21 01:22:35 by aaheddar          #+#    #+#             */
+/*   Updated: 2026/08/21 01:22:36 by aaheddar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../header/core.h"
 
-static t_time *get_start_time(void)
+static t_time	*get_start_time(void)
 {
-    static t_time start_time;
+	static t_time	start_time;
 
-    return (&start_time);
+	return (&start_time);
 }
 
-void set_start_time(void)
+void	set_start_time(void)
 {
-    gettimeofday(get_start_time(), NULL);
+	gettimeofday(get_start_time(), NULL);
 }
 
-long gettimestamp(void)
+long	gettimestamp(void)
 {
-    t_time *start_time;
-    t_time present_time;
+	t_time	*start_time;
+	t_time	present_time;
 
-    start_time = get_start_time();
-    gettimeofday(&present_time, NULL);
-
-    return ((present_time.tv_sec - start_time->tv_sec) * 1000
-        + (present_time.tv_usec - start_time->tv_usec) / 1000);
+	start_time = get_start_time();
+	gettimeofday(&present_time, NULL);
+	return ((present_time.tv_sec - start_time->tv_sec) * 1000
+		+ (present_time.tv_usec - start_time->tv_usec) / 1000);
 }
